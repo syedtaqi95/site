3862c6bc141a1c3716fd7036f457af7aa33502c2
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-08-01 15:21:41 +0100

    Added navbar, styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,23 @@
 import type { AppProps } from 'next/app'
 import Head from "next/head";
-import 'bootstrap/dist/css/bootstrap.css'
+import Script from 'next/script'
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=dvice-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Bootstrap CSS CDN */}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
       </Head>
+      
       <Component {...pageProps} />
+      
+      {/* Bootstrap JS CDN */}
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></Script>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,57 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Home = () => {
   return (
-    <div className="container mx-auto py-4">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-md navbar-light">
+        <div className="container my-2">
+
+          <span className="navbar-brand">
+            <a href="mailto:syed.taqi95@gmail.com" className="nav-icon mx-3" rel="noreferrer" target="_blank">
+              <i className="bi bi-envelope" />
+            </a>
+            <a href="https://www.linkedin.com/in/syedtaqi-haider/" className="nav-icon mx-3" rel="noreferrer" target="_blank">
+              <i className="bi bi-linkedin" />
+            </a>
+            <a href="https://github.com/syedtaqi95" className="nav-icon mx-3" rel="noreferrer" target="_blank">
+              <i className="bi bi-github" />
+            </a>
+          </span>
+
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item mx-2">
+                <Link href="/">
+                  <a><h5>Home</h5></a>
+                </Link>
+              </li>
+              <li className="nav-item mx-2">
+                <Link href="#about">
+                  <a><h5>About</h5></a>
+                </Link>
+              </li>
+              <li className="nav-item mx-2">
+                <Link href="#contact">
+                  <a><h5>Get in touch</h5></a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </nav>
+
+      <div className="container">
+        <h1>Hi, I'm Syed.</h1>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid voluptatibus dolorem voluptate nostrum quasi soluta, numquam incidunt sed ullam dolor ea repudiandae, accusamus a maiores error aut nemo quisquam hic?
+      </div>
+    </>
   )
 }
 
