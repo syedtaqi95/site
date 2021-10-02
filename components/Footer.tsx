ad5255a4d846185eda825902a746f97d08ddafdf
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-10-02 12:08:43 +0100

    Modified Footer typography

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { Text, Divider } from "@chakra-ui/react";
+import { Text, Divider, useColorModeValue } from "@chakra-ui/react";
 
 const Footer = () => {
+  const hlColour = useColorModeValue("blue.600", "cyan.600");
   return (
     <>
       <Divider pt={20} colorScheme="blackAlpha" />
       <Text textAlign="center">
-        Built with <strong>Next.js</strong> and <strong>Chakra UI</strong>.
-        Hosted on <strong>Vercel</strong>.<br />© Syed Taqi Haider. All Rights
-        Reserved.
+        Built with{" "}
+        <Text as="span" color={hlColour} fontWeight="bold">
+          Next.js
+        </Text>{" "}
+        and{" "}
+        <Text as="span" color={hlColour} fontWeight="bold">
+          Chakra UI
+        </Text>
+        . Hosted on{" "}
+        <Text as="span" color={hlColour} fontWeight="bold">
+          Vercel
+        </Text>
+        .<br />© Syed Taqi Haider. All Rights Reserved.
       </Text>
     </>
   );
