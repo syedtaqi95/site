2018c013114930d57f1050fe9d6cc9016a26ede0
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-08-01 23:47:36 +0100

    Added Hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <section className="flex flex-col items-start justify-center mx-40 h-[94vh]">
+      <h1 className="font-bold text-6xl">Hello world, I'm Syed 👋</h1>
+      <h2 className="text-2xl mt-8">
+        Software Developer /sɒftwɛr dɪˈvɛləpər/
+      </h2>
+      <h2 className="italic">noun</h2>
+      <h2>
+        A magical creature who turns caffeine into code and dreams in lines of
+        syntax.
+      </h2>
+    </section>
+  );
+};
+
+export default Hero;
