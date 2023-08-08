d009fca011f64dd355b1b829fdf9929497682a10
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-08-08 01:53:52 +0100

    Added sidebar

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const NavListItem = ({ title, href }: { title: string; href: string }) => {
   return (
     <li className="inline">
-      <a href={href} className="group">
+      <a href={href} className="group text-lg md:text-base">
         {title}
         <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black" />
       </a>
@@ -14,6 +14,7 @@ const NavListItem = ({ title, href }: { title: string; href: string }) => {
 
 const NavigationMenu = () => {
   const [isMenuOpen, setMenuIsOpen] = useState<boolean>(false);
+
   return (
     <header className="px-6 md:px-10 lg:px-14 py-3 sticky top-0 backdrop-blur">
       <nav className="flex items-center justify-between">
@@ -21,6 +22,7 @@ const NavigationMenu = () => {
           <img alt="logo" src="apple-touch-icon.png" className="w-10 h-10" />
         </a>
 
+        {/* Displayed on wider screen widths */}
         <ol className="hidden md:flex justify-between gap-8">
           {navLinks.map(({ title, href }) => (
             <NavListItem key={title} title={title} href={href} />
@@ -30,17 +32,14 @@ const NavigationMenu = () => {
           </li>
         </ol>
 
+        {/* Displayed on smaller screen widths */}
         <button
-          className={"flex md:hidden relative group"}
+          className="flex md:hidden z-10"
           onClick={() => {
             setMenuIsOpen(!isMenuOpen);
           }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30">
             <line
               x1="0"
               y1="50%"
@@ -70,6 +69,19 @@ const NavigationMenu = () => {
             />
           </svg>
         </button>
+
+        <aside
+          autoFocus
+          className={`${
+            isMenuOpen ? "block -translate-x-0" : "translate-x-full"
+          } transition-transform duration-300 md:hidden fixed right-0 top-0 h-screen w-1/3 bg-sky-300 `}
+        >
+          <ol className="flex flex-col items-center justify-center gap-8 w-full p-3 mt-28">
+            {navLinks.map(({ title, href }) => (
+              <NavListItem key={title} title={title} href={href} />
+            ))}
+          </ol>
+        </aside>
       </nav>
     </header>
   );
