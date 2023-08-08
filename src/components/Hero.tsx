b7e11991f0b414d90f2ea5c1793f427d28e782f7
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-08-08 07:56:32 +0100

    Updated Hero styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,14 @@
 const Hero = () => {
   return (
-    <section className="flex flex-col items-center justify-center h-[92vh] px-4 md:px-14">
+    <section className="flex flex-col items-center justify-center h-[92vh] px-6 md:px-24 lg:px-32">
       <div>
         <p className="font-mono text-lg font-medium text-teal-700 dark:text-green-350 mb-2">
           Hello world, I am
         </p>
-        <h1 className="text-4xl md:text-6xl lg:text-8xl font-extrabold py-2 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-600">
+        <h1 className="text-2xl md:text-4xl lg:text-6xl font-extrabold py-2 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-600">
           Syed Taqi Haider.
         </h1>
-        <h1 className="text-4xl md:text-6xl lg:text-8xl font-extrabold py-2 mb-4">
+        <h1 className="text-2xl md:text-4xl lg:text-6xl font-extrabold py-2 mb-4">
           I turn caffeine into lines of code.
         </h1>
         <p className="max-w-prose">
@@ -16,7 +16,7 @@ const Hero = () => {
           stack - from low-power embedded devices to scalable full-stack web
           applications running in the cloud. Currently, I'm developing software
           for the next generation of innovative laboratory instruments at{" "}
-          <a href="https://cambustion.com">
+          <a href="https://cambustion.com" target="_blank">
             <span className="text-teal-700 hover:text-teal-500 dark:text-green-350 dark:hover:text-emerald-500">
               Cambustion
             </span>
