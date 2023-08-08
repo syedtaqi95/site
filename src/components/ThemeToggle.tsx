72f17b8d5d34b32667897c77b5b469af56223d28
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-08-08 07:12:36 +0100

    Fixed dark mode client-server mismatch

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const ThemeToggle = ({
   theme,
   setTheme,
@@ -5,6 +7,16 @@ const ThemeToggle = ({
   theme: string;
   setTheme: React.Dispatch<React.SetStateAction<string>>;
 }) => {
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
       {theme === "dark" ? (
@@ -28,9 +40,7 @@ const ThemeToggle = ({
           height="24"
           viewBox="0 0 256 256"
         >
-          <path
-            d="M240 96a8 8 0 0 1-8 8h-16v16a8 8 0 0 1-16 0v-16h-16a8 8 0 0 1 0-16h16V72a8 8 0 0 1 16 0v16h16a8 8 0 0 1 8 8Zm-96-40h8v8a8 8 0 0 0 16 0v-8h8a8 8 0 0 0 0-16h-8v-8a8 8 0 0 0-16 0v8h-8a8 8 0 0 0 0 16Zm72.77 97a8 8 0 0 1 1.43 8A96 96 0 1 1 95.07 37.8a8 8 0 0 1 10.6 9.06a88.07 88.07 0 0 0 103.47 103.47a8 8 0 0 1 7.63 2.67Zm-19.39 14.88c-1.79.09-3.59.14-5.38.14A104.11 104.11 0 0 1 88 64c0-1.79 0-3.59.14-5.38a80 80 0 1 0 109.24 109.24Z"
-          />
+          <path d="M240 96a8 8 0 0 1-8 8h-16v16a8 8 0 0 1-16 0v-16h-16a8 8 0 0 1 0-16h16V72a8 8 0 0 1 16 0v16h16a8 8 0 0 1 8 8Zm-96-40h8v8a8 8 0 0 0 16 0v-8h8a8 8 0 0 0 0-16h-8v-8a8 8 0 0 0-16 0v8h-8a8 8 0 0 0 0 16Zm72.77 97a8 8 0 0 1 1.43 8A96 96 0 1 1 95.07 37.8a8 8 0 0 1 10.6 9.06a88.07 88.07 0 0 0 103.47 103.47a8 8 0 0 1 7.63 2.67Zm-19.39 14.88c-1.79.09-3.59.14-5.38.14A104.11 104.11 0 0 1 88 64c0-1.79 0-3.59.14-5.38a80 80 0 1 0 109.24 109.24Z" />
         </svg>
       )}
     </button>
