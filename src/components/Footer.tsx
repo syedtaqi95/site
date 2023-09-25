013870d0cc5649f97253e03d60895f953d080e52
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-09-25 20:12:01 +0100

    Added CC BY-NC 4.0 licence in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Link from "./shared/Link";
 
 const Footer = () => {
   return (
     <footer className="mx-6 md:mx-24 lg:mx-32 max-w-6xl font-sans py-8 border-t border-slate-200 dark:border-light-navy text-xs">
-      <p>© Syed Taqi Haider 2023. All Rights Reserved.</p>
+      <p>
+        <Link href="https://creativecommons.org/licenses/by-nc/4.0/">
+          CC BY-NC 4.0
+        </Link>{" "}
+        © Syed Taqi Haider 2023.
+      </p>
     </footer>
   );
 };
